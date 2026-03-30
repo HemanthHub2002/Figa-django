81fda9063b5afd9c695ddc4506415686ba606e40
HemanthHub2002 <hemanthkgr2002@gmail.com>
2026-03-30 23:28:47 +0530

    Follow user troubleshooting guide for Vercel deployment: update vercel.json, settings, and requirements.

--- Eshop/Eshop/settings.py
+++ Eshop/Eshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-pdcbuw#7d89qr!n-3v@$vgc5--%x1vo99sv^79z2tn=&@a$iwm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Eshop/build_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt
-echo "Collecting static files... (from root)"
-python3 manage.py collectstatic --noinput --clear
